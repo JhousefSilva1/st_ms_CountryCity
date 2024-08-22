5d49b9374043c1651e7f331438892e76f7e73c13
Jose Pozo Silva <jhosede23@gmail.com>
2024-08-21 20:24:53 -0400

    adding tolls crud

---
+++ src/main/java/com/smart/tolls/ucb/edu/bo/SmartTolls_CountryCityService/entity/StTollsEntity.java
@@ -0,0 +1,81 @@
+package com.smart.tolls.ucb.edu.bo.SmartTolls_CountryCityService.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "st_tolls")
+public class StTollsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "st_tolls_id")
+    private Long stTollsId;
+
+    @Column(name = "st_tolls_tolls", nullable = false)
+    private String stTollsTolls;
+
+    @Column(name = "st_origin_places_st_places_id", nullable = false)
+    private Long stOriginPlacesStPlacesId;
+
+    @Column(name = "st_destination_places_st_places_id", nullable = false)
+    private Long stDestinationPlacesStPlacesId;
+
+    @Column(name = "st_tolls_cost", nullable = false)
+    private Double stTollsCost;
+
+    @Column(name = "st_road_type_st_road_type_id", nullable = false)
+    private Long stRoadTypeStRoadTypeId;
+
+    @Column(name = "createdby",nullable = false)
+    private String createdBy;
+
+    @Column(name = "createddate",nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "modifiedby",nullable = false)
+    private String modifiedBy;
+
+    @Column(name = "modifieddate",nullable = false)
+    private LocalDateTime modifiedDate;
+
+
+    @Column(name = "isdeleted",nullable = false)
+    private Boolean isDeleted;
+
+    @Column(name = "deletedby",nullable = false)
+    private String deletedBy;
+
+    @Column(name = "deleteddate",nullable = false)
+    private LocalDateTime deletedDate;
+
+    @Column(name = "ipaddress",nullable = false)
+    private String ipAddress;
+
+    @Column(name = "action",nullable = false)
+    private String action;
+
+    @Column(name = "previousvalue",nullable = false)
+    private String previousValue;
+
+    @Column(name = "currentvalue",nullable = false)
+    private String currentValue;
+
+    @Column(name = "recordversion",nullable = false)
+    private Integer recordVersion;
+
+    @Column(name = "operationtype",nullable = false)
+    private String operationType;
+
+    @Column(name = "role",nullable = false)
+    private String role;
+
+
+}
