233c8feb35294418a6faa82b929f1ab07b0870ea
JhousefSilva1 <jhosede23@gmail.com>
2025-01-04 12:41:45 -0400

    adding entities

---
+++ src/main/java/com/smart/tolls/ucb/edu/bo/SmartTolls_CountryCityService/entity/StRoadType.java
@@ -0,0 +1,22 @@
+package com.smart.tolls.ucb.edu.bo.SmartTolls_CountryCityService.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "st_road_type")
+public class StRoadType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "st_road_type_id")
+    private Long idRoadType;
+
+    private String roadType;
+
+}
